e7a503c5460ed06b070d9cc15a10cf89a817ae64
jashawnyu <jashawnyu@gmail.com>
2023-11-12 13:50:45 +0800

    vimwiki
    vimwiki configuration

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,24 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
+    'vimwiki/vimwiki',
+    event = "BufEnter *.md",
+    keys = {"<leader>ww", "<leader>wt"},
+    enabled = true,
+    init = function () --replace 'config' with 'init'
+      vim.g.vimwiki_list = {{path = '~/Notes', syntax = 'markdown', ext = '.md'}}
+      vim.g.vimwiki_listsyms = " ◔◑◕✓"
+      -- vim.api.nvim_set_keymap('n', '<C-]>', '<Plug>VimwikiNextLink', {silent=true})
+      -- vim.api.nvim_set_keymap('n', '<C-[>', '<Plug>VimwikiPrevLink', {silent=true})
+      --vim.api.nvim_set_keymap('n', '<S-Tab>', '<Plug>VimwikiDecreaseLvlWholeItem', {silent=true})
+      --vim.api.nvim_set_keymap('n', '<Tab>', '<Plug>VimwikiIncreaseLvlWholeItem', {silent=true})
+    end,
+  },
 }
